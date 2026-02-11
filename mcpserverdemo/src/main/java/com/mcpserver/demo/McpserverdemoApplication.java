c2ec25e66adeffe169bda8bed774329807424d2e
ray.yau <a05166ray@gmail.com>
2026-02-11 17:40:20 +0800

    first commit

---
+++ mcpserverdemo/src/main/java/com/mcpserver/demo/McpserverdemoApplication.java
@@ -0,0 +1,13 @@
+package com.mcpserver.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpserverdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpserverdemoApplication.class, args);
+	}
+
+}
